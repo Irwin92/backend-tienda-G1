d1e838810b3f7f572786e7f104d1d092af160783
Irwin92 <irwinleal18@gmail.com>
2026-09-17 08:29:14 -0600

    añadir tokens

--- backend_tienda/settings.py
+++ backend_tienda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vu+$ifb#%9kb^d4_h)-mfl-+hfg^cpoin0jpr_t74-gz6$1fj0'
+SECRET_KEY = 'django-insecure-kmd8b33i6o@_mdqcrrg6h-99_+ojo*c*w*&%0^v^xm(4@)z=!e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'productos',
 ]
 
--- productos/serializers.py
+++ productos/serializers.py
@@ -1,40 +1,42 @@
 from rest_framework import serializers
 from .models import Producto, Categoria
 
-# model= Producto indica que modelo se transformara y validara
-# fields= '__all__'exponde todos los campos del modelo
+# modelo = Producto  Indica que modelo se transformara y validara
+# fields= '__all__'  Expone todos los campos del modelo
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields= '__all__'
 
-    def validate_precio(self, value):
+    def validate_precio(self,value):
         if value <=0:
             raise serializers.ValidationError(
-                'El precio debe ser mayor que cero.'
-            )
+                'El precio debe ser mayor que cero'
+                )
         return value
-
-    def validate_stock(self, value):
+    
+    def validate_stock(self,value):
             if value <0:
                 raise serializers.ValidationError(
-                    'El stock no debe ser negativo.'
-                )
+                    'El stock no puede ser negativo.'
+                    )
             return value
-#-----------------------------------------------------------------------------------------
-
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields= '__all__'
 
-    def validate_nombre(self, value):
-        if value is None:
-            raise serializers.ValidationError(
-                'El nombre no debe ser vacio.'
-            )
-        return value
+    def validate_nombre(self,value):
 
+        nombre_limpio= value.strip()
+        
+        if len(nombre_limpio) < 3:
+            raise serializers.ValidationError(
+                'El nombre debe tener al menos 3 caracteres.'
+                )
+        return nombre_limpio
    
-    
-    
+
+
+
